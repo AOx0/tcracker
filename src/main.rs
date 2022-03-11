c85b1152b45e6b788da66dd2fc73cc707bfb2954
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-03-10 20:51:43 -0600

    Support T***  1.0.0~1.1.5
        - Improve prints
        - Reduce bytes pattern array from 27 to 10.
        - Allow to execute for newer versions after a confirmation prompt
        - Update dependencies
        - Upgrade version to 0.2.0
        - Now supports <=1.1.5

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
+use std::io;
 use std::process::exit;
 
 mod args;
 mod version;
 mod bytes;
+
+macro_rules! printf {
+    ( $($t:tt)* ) => {
+        {
+            use std::io::Write;
+            let mut h = std::io::stdout();
+            write!(h, $($t)* ).unwrap();
+            h.flush().unwrap();
+        }
+    }
+}
+
+macro_rules! eprintf {
+    ( $($t:tt)* ) => {
+        {
+            use std::io::Write;
+            let mut h = std::io::stderr();
+            write!(h, $($t)* ).unwrap();
+            h.flush().unwrap();
+        }
+    }
+}
+
 
 #[tokio::main]
 async fn main() {
@@ -13,30 +37,40 @@ async fn main() {
     let version = version::Version::get(&path).await;
     let binary = format!("{}/Contents/MacOS/{}", path, app);
 
-    if version.0 <= 1 && version.1 <= 0 && version.2 <= 5 {
-        let mut b = bytes::Bytes::get_bytes(&binary);
-        if let Some(address) = b.get_f_address() {
-            println!("Found address at: 0x{}", format!("{:x}", address).to_uppercase());
-            let address = address + 27;
+    if !(version.0 <= 1 && version.1 <= 1 && version.2 <= 5) {
+        printf!("This tool was proved up to version 1.0.5\nDo you want to continue for version {}.{}.{}? [y/n]: ", version.0, version.1, version.2);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("error: unable to read user input");
+        input = input.replace(" ", "").replace("\n", "").trim().parse().unwrap();
+        if input != "y" {
+            printf!("Ending program...\n");
+            exit(0);
+        }
+    }
+    printf!("Trying to crack ...\n");
 
-            if  b.0[address] == 0x85 && b.0[address + 2] == 0x74 {
-                println!("{}: {:02x} -> 89 ", address,  b.0[address]);
-                println!("{}: {:02x} -> eb", address+2, b.0[address + 2]);
+    let mut b = bytes::Bytes::get_bytes(&binary);
+    if let Some(address) = b.get_f_address() {
+        printf!("    [*] Found address at: 0x{}\n", format!("{:x}", address).to_uppercase());
+        let address = address + 10;
 
-                b.0[address] = 0x89;
-                b.0[address + 2] = 0xEB;
+        if  b.0[address] == 0x85 && b.0[address + 2] == 0x74 {
+            printf!("    [*] {}: {:02x} -> 89 \n", address,  b.0[address]);
+            printf!("    [*] {}: {:02x} -> eb \n", address+2, b.0[address + 2]);
 
-                b.write();
-            } else if b.0[address] == 0x89 && b.0[address + 2] == 0xEB {
-                eprintln!("Already patched");
-                exit(1);
-            } else {
-                eprintln!("Found unexpected bytes: 0x{:x}, 0x{:x}", b.0[address], b.0[address + 2]);
-                exit(1);
-            }
+            b.0[address] = 0x89;
+            b.0[address + 2] = 0xEB;
+
+            b.write();
+        } else if b.0[address] == 0x89 && b.0[address + 2] == 0xEB {
+            eprintf!("    [x] Error: Already patched\n");
+            exit(1);
         } else {
-            eprintln!("Failed to find pattern");
+            eprintf!("    [x] Error: Found unexpected bytes: 0x{:x}, 0x{:x}\n", b.0[address], b.0[address + 2]);
             exit(1);
         }
+    } else {
+        eprintf!("    [x] Error: Failed to find pattern\n");
+        exit(1);
     }
 }
